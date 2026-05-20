de0d8643be75cf92fc780c9c7fd006bec617421e
BuildTools <unconfigured@null.spigotmc.org>
2026-05-20 17:08:28 +0300

    finished start_timer (along with countdown logic), stop_timer and reset_timer logic

--- timer_window.py
+++ timer_window.py
@@ -2,11 +2,17 @@ import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap import Style
 
+POPUP_COUNT = 0
+
 class TimerWindow:
     def __init__(self):
         self.root = tk.Tk()
+        res_width = self.root.winfo_screenwidth()
+        res_height = self.root.winfo_screenheight()
+        x = int(0.5 * res_width - 150)
+        y = int(0.5 * res_height - 100)
         self.root.title("Timer")
-        self.root.geometry("300x200")
+        self.root.geometry(f"300x200+{x}+{y}")
         self.root.configure(bg="gray")
         self.style = Style(theme="darkly")
         self.style.theme_use()
@@ -47,17 +53,25 @@ class TimerWindow:
         self.minutes_var = tk.StringVar(value="00")
         self.seconds_var = tk.StringVar(value="00")
 
+        self._picker = None
+
     def show_picker(self, button, values, on_select):
-        if getattr(self, "_picker", None) and self._picker.winfo_exists():
+        global POPUP_COUNT
+        POPUP_COUNT += 1
+        if POPUP_COUNT > 1:
             self._picker.destroy()
+            POPUP_COUNT -= 1
 
         popup = tk.Toplevel(self.root)
-        popup.withdraw()
-        popup.transient(self.root)
+        self._picker = popup
+        #popup.withdraw()
+        #popup.transient(self.root)
         popup.overrideredirect(True)
 
         x = button.winfo_rootx()
         y = button.winfo_rooty() + button.winfo_height()
+        popup.geometry(f"50x120+{x}+{y}")
+
 
         frame = ttk.Frame(popup)
         frame.pack(fill="both", expand=True)
@@ -71,21 +85,22 @@ class TimerWindow:
         for value in values:
             listbox.insert("end", value)
 
-        popup.update_idletasks()
+        #popup.update_idletasks()
 
-        popup.geometry(f"50x120+{x}+{y}")
 
-        popup.deiconify()
+        #popup.deiconify()
         #popup.lift()
         #listbox.focus_set()
 
         def on_pick(event):
+            global POPUP_COUNT
             selection = listbox.curselection()   
             if not selection:
                 return
 
             on_select(listbox.get(selection[0]))
             popup.destroy()
+            POPUP_COUNT -= 1
 
         listbox.bind("<<ListboxSelect>>", on_pick)
 
@@ -95,11 +110,11 @@ class TimerWindow:
 
     def select_minutes(self):
         values = [f"{h:02d}" for h in range(60)]
-        self.show_picker(self.hours_button, values, self.set_minutes)
+        self.show_picker(self.minutes_button, values, self.set_minutes)
 
     def select_seconds(self):
         values = [f"{h:02d}" for h in range(60)]
-        self.show_picker(self.hours_button, values, self.set_seconds)
+        self.show_picker(self.seconds_button, values, self.set_seconds)
 
     def set_hours(self, value):
         self.hours_var.set(value)
@@ -114,13 +129,60 @@ class TimerWindow:
         self.seconds_button.configure(text=value)
 
     def start_timer(self):
-        pass
+        if getattr(self, 'running', False): 
+            return
+        
+        seconds = int(self.seconds_var.get())
+        minutes = int(self.minutes_var.get())
+        hours = int(self.hours_var.get())
+
+        self.range_val = hours * 3600 + minutes * 60 + seconds
+
+        if self.range_val > 0:
+            self.running = True
+            self.countdown()
+
+
+    def countdown(self):
+        if not getattr(self, 'running', False): 
+            return
+
+        if self.range_val > 0:
+            self.range_val -= 1
+
+            h = self.range_val // 3600
+            m = (self.range_val % 3600) // 60
+            s = self.range_val % 60
+
+            self.hours_button.config(text=f"{h:02d}")
+            self.minutes_button.config(text=f"{m:02d}")
+            self.seconds_button.config(text=f"{s:02d}")
+
+            self.root.after(1000, self.countdown)
+
+        else:
+            self.running = False
 
     def stop_timer(self):
-        pass
+        self.running = False
+        h = self.range_val // 3600
+        m = (self.range_val % 3600) // 60
+        s = self.range_val % 60
+
+        self.hours_var.set(h)
+        self.minutes_var.set(m)
+        self.seconds_var.set(s)
 
     def reset_timer(self):
-        pass
+        self.running = False
+
+        self.hours_var.set("00")
+        self.minutes_var.set("00")
+        self.seconds_var.set("00")
+
+        self.hours_button.config(text="00")
+        self.minutes_button.config(text="00")
+        self.seconds_button.config(text="00")
 
     def main(self):
         self.root.mainloop()
