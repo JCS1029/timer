9a0d150ebff4f76698ed52dcf5b5ef2a82d1ac37
username <lovely.m.2004@gmail.com>
2026-07-01 11:22:19 +0300

    worked on the custom page

--- custom_mode.py
+++ custom_mode.py
@@ -5,6 +5,10 @@ from ttkbootstrap import Style
 class CustomMode(tk.Frame):
     def __init__(self, master, on_back=None):
         tk.Frame.__init__(self, master)
+
+        self.style = Style()        
+        self.style.configure('LabelStyle.TLabel', font=('Arial', 12))
+
         self.header = tk.Frame(self)
         self.header.pack(fill="x", anchor="n", pady=5)
 
@@ -17,8 +21,26 @@ class CustomMode(tk.Frame):
         self.frame = tk.Frame(self)
         self.frame.pack(fill="x", pady=15)
 
-        self.sessionName = tk.Entry(self.frame)
-        self.sessionName.pack(side="left", padx=15, pady=10)
-        
+        self.modeName = ttk.Label(self.frame, text="Mode Name", style="LabelStyle.TLabel")
+        self.modeName.grid(row=0, column=0, padx=(60, 20))
 
+        self.modeNameInput = tk.Entry(self.frame)
+        self.modeNameInput.grid(row=1, column=0, padx=(60, 20), pady=15)
         
+        self.sessionTime = ttk.Label(self.frame, text="Session Duration", style="LabelStyle.TLabel")
+        self.sessionTime.grid(row=0, column=1, padx=20)
+
+        self.sessionTimeInput = tk.Entry(self.frame)
+        self.sessionTimeInput.grid(row=1, column=1, padx=20, pady=15)
+        
+        self.breakTime = ttk.Label(self.frame, text="Break Duration", style="LabelStyle.TLabel")
+        self.breakTime.grid(row=0, column=2, padx=20)
+
+        self.breakTimeInput = tk.Entry(self.frame)
+        self.breakTimeInput.grid(row=1, column=2, padx=20, pady=15)
+
+        self.sessionCount = ttk.Label(self.frame, text="Number of sessions", style="LabelStyle.TLabel")
+        self.sessionCount.grid(row=0, column=3, padx=(20, 60))
+
+        self.sessionCountInput = tk.Entry(self.frame)
+        self.sessionCountInput.grid(row=1, column=3, padx=(20, 60), pady=15)
